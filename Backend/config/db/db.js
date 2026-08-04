b7f273115f53dd7ba35728ae77e72288cfe29f98
Kuwarji <154698664+Kuwarjibetha@users.noreply.github.com>
2026-08-04 15:34:06 +0530

    Add configurable database port

--- Backend/config/db/db.js
+++ Backend/config/db/db.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
     logging: false, // set to console.log if you want to see raw SQL queries
   }
--- Backend/service/v1/index.js
+++ Backend/service/v1/index.js
@@ -5,13 +5,6 @@ const departmentService = require('./department/department.service');
 const notificationService = require('./notification/notification.service');
 const requestService = require('./request/request.service');
 const userService = require('./user/user.service');
-const approverService = require('./approver/approver.service');
-const auditService = require('./audit/audit.service');
-const checkerService = require('./checker/checker.service');
-const departmentService = require('./department/department.service');
-const notificationService = require('./notification/notification.service');
-const requestService = require('./request/request.service');
-const userService = require('./user/user.service');
 const workflowService = require('./workflow/workflow.service');
 
 // service exports
